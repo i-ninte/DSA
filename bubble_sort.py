e66a1678917fa3434663086645349ea8a75a8e8a
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2023-11-25 14:40:35 +0000

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,1 +1,15 @@
+def bubble_sort(list):
+    unsorted_index= len(list)-1
+    sorted= False
+    while not sorted:
+        sorted=True
+        for i in range(unsorted_index):
+            if list[i] > list[i+1]:
+                sorted= False
+                list[i],list[i+1]=list[i+1],list[i]
+        unsorted_index= unsorted_index-1
 
+
+list= [65,55,45,35,25,15,10]
+bubble_sort(list)
+print(list)
