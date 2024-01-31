a9792882d3e23f67fe49eadce49bc148c46beafc
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-01-31 08:11:10 +0000

    Update solutions.py

--- hackerrank interview prep/warm up/jumping on the clouds/solutions.py
+++ hackerrank interview prep/warm up/jumping on the clouds/solutions.py
@@ -1,1 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # number of jumps
+    jump =0 
+    # to track the index
+    i=0
+    
+    while i < n-1:
+        if i+2<n and c[i+2]==0:
+            jump+=1
+            i+=2
+        elif i+1<n and c[i+1]==0:
+            jump+=1
+            i+=1
+            
+    return jump
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
 
