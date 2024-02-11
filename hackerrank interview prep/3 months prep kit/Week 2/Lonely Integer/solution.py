f54db09b366fe2a58229a65e5e14f4e7cd64d4d4
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-02-11 19:58:19 +0000

    Update solution.py

--- hackerrank interview prep/3 months prep kit/Week 2/Lonely Integer/solution.py
+++ hackerrank interview prep/3 months prep kit/Week 2/Lonely Integer/solution.py
@@ -1,1 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    unique_element = 0
+    for num in a:
+      #using the XOR to return unique elements
+        unique_element ^= num
+    return unique_element
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
 
