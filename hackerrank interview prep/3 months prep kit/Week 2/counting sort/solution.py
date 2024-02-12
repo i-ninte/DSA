2a8c174f02b40cd5cc4570c3218b28d8823f79b8
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-02-12 10:19:07 +0000

    Update solution.py

--- hackerrank interview prep/3 months prep kit/Week 2/counting sort/solution.py
+++ hackerrank interview prep/3 months prep kit/Week 2/counting sort/solution.py
@@ -1,1 +1,39 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    # Create an array to store the frequency of each value
+    result = [0] * 100
+    
+    # Iterate through the input array and count the occurrences of each value
+    for num in arr:
+        result[num] += 1
+    
+    return result
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
