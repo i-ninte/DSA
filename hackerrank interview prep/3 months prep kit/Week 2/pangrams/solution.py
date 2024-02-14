c6619b5cfa05f61bb2618d0a39ffc40df4cc1a18
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-02-14 08:44:07 +0000

    Update solution.py

--- hackerrank interview prep/3 months prep kit/Week 2/pangrams/solution.py
+++ hackerrank interview prep/3 months prep kit/Week 2/pangrams/solution.py
@@ -1,1 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabets:
+        if char not in s.lower():
+            return "not pangram"
+            
+         
+    return "pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
 
