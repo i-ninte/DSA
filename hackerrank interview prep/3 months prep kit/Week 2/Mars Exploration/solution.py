8cfee193c7d3f3aacda1147347894a19ddccda2c
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-02-18 11:50:40 +0000

    Update solution.py

--- hackerrank interview prep/3 months prep kit/Week 2/Mars Exploration/solution.py
+++ hackerrank interview prep/3 months prep kit/Week 2/Mars Exploration/solution.py
@@ -1,1 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
 
